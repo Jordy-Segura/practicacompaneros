a3699e84422ebc7eb97a3e6fbeb7e380f3602839
jordy <marcelo2005jmsp@gmail.com>
2024-11-20 15:23:17 -0500

    holi

---
+++ compas/src/main/java/ec/edu/espoch/compas/Compas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.compas;
+
+/**
+ *
+ * @author jordy
+ */
+public class Compas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
