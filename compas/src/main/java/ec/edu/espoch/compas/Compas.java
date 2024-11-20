169ca2494080acf7384321b43e899a91cef495b3
Dilankl30 <lucerodk7@gmail.com>
2024-11-20 15:39:10 -0500

    actualizacion

--- compas/src/main/java/ec/edu/espoch/compas/Compas.java
+++ compas/src/main/java/ec/edu/espoch/compas/Compas.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ec.edu.espoch.compas;
 
 /**
@@ -12,5 +11,8 @@ public class Compas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("hola");
+        System.out.println("holas crack"
+        );
     }
 }
